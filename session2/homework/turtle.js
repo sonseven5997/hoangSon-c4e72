eea1f92f339109c29558d35ab9d4b9356add06db
sonseven5997 <sonseven5997@gmail.com>
2020-02-15 00:03:26 +0700

    homework-session2

---
+++ session2/homework/turtle.js
@@ -0,0 +1,15 @@
+clear()
+setshape('car')
+let x = prompt('Enter number of edges')
+for(let i=0;i<x;i++){
+  fd(20)
+  lt(360/x)}
+
+  
+clear()
+setshape('car')
+let n = Number(prompt('How many polygons?'))
+for(let j=3;j<n+3;j++){
+  for(let i=0;i<j;i++){
+    fd(50)
+    lt(360/j)}}
