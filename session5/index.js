2e6cb85e6a1b96f19928cfbe18b73f33cca19d6d
sonseven5997 <sonseven5997@gmail.com>
2020-02-24 22:21:55 +0700

    session5

---
+++ session5/index.js
@@ -0,0 +1,52 @@
+// // chi goi ham de xu ly logic, khong dung de in ra
+// function calc(a, b,  op){
+//     if (op == '+'){
+//         return a + b
+//     } else if (op == '-'){
+//         return a - b
+//     } else if (op == '*'){
+//         return a * b
+//     } else if (op == '/'){
+//         return a / b
+//     }
+// }
+
+
+// let a = Math.floor(Math.random() * 10)
+// let b = Math.floor(Math.random() * 10)
+// let opPoll = ['+', '-', '*', '/']
+// let op = opPoll[Math.floor(Math.random() * 5)]
+// if (op == '/' && b == 0){
+//     // your code here
+// }
+// let c = calc(a, b, op)
+// let numberPoll = [-1, 0, 1]
+// let wrongUnit = numberPoll[Math.floor(Math.random() * 3)]
+// let d = c + wrongUnit
+// console.log(`${a} ${op} ${b} = ${d}`)
+// // let c = Math.random()
+// // console.log(c)
+// // if (c<0.5){
+// //     console.log(`${a} + ${b} = ${a+b}`)
+// // }
+// // else if(0.5<=c<0.75) {
+// //     console.log(`${a} + ${b} = ${a+b+1}`)
+// // }
+// // else {
+// //     console.log(`${a} + ${b} = ${a+b-1}`)
+// // }
+
+// // let choose = prompt('Moi ban chon T/F').toUpperCase()
+
+// // if (choose === 'T' || choose === 'F'){
+// //     if ((choose === "T" && (c<0.5)) || (choose === 'F' && (c>=0.5))){
+// //         console.log('Ban da chon dung')
+// //     }
+// //     else {
+// //         console.log('Ban da chon sai')
+// //     }
+// // }
+
+// // else {
+// //     console.log('Vui long chi chon T/F')
+// // }
